86b4eb870d09370d8523f12a1514f3853ad1bd9d
honzi <czechinthemail@gmail.com>
2016-12-27 21:47:33 -0800

    removed unnecessary ceil

--- js/levels.js
+++ js/levels.js
@@ -5,8 +5,8 @@ function load_level(id){
     var loop_counter = settings_settings['hexagons'] - 1;
     do{
         var position = select_hexagon(
-          Math.ceil(random_integer(settings_settings['width']) - settings_settings['width'] / 2),
-          Math.ceil(random_integer(settings_settings['height']) - settings_settings['height'] / 2)
+          random_integer(settings_settings['width']) - settings_settings['width'] / 2,
+          random_integer(settings_settings['height']) - settings_settings['height'] / 2
         );
 
         create_hexagon(
