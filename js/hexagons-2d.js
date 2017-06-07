515be00ad48136dccf4d8fff400a99a82b99a3ef
honzi <czechinthemail@gmail.com>
2017-06-07 14:19:44 -0700

    added wrap div around canvas/webgl

--- js/hexagons-2d.js
+++ js/hexagons-2d.js
@@ -534,7 +534,7 @@ function setmode_logic(newgame){
 
     // Main menu mode.
     if(canvas_mode === 0){
-        document.body.innerHTML = '<div><div><a onclick=canvas_setmode({mode:1,newgame:true})>New Game</a></div></div>'
+        document.getElementById('wrap').innerHTML = '<div><div><a onclick=canvas_setmode({mode:1,newgame:true})>New Game</a></div></div>'
           + '<div class=right><div><input id=camera-keys maxlength=4>Camera ↑←↓→<br>'
           + '<input id=delete-player>Delete Player<br>'
           + '<input id=end-turn-key>End Turn<br>'
