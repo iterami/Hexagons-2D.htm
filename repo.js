1b958c4301066793ffe0ddc69225f6d0fe036746
Jan Hořava <czechinthemail@gmail.com>
2026-02-18 11:13:21 -0800

    mediumized start button

--- repo.js
+++ repo.js
@@ -384,7 +384,7 @@ function repo_init(){
         'y_scaled_double': 0,
         'y_scaled_half': 0,
       },
-      'info': '<button id=start type=button>Start New Game</button><button id=reset_camera type=button>Reset Camera</button>',
+      'info': '<button class=medium id=start type=button>Start New Game</button><button id=reset_camera type=button>Reset Camera</button>',
       'menu': true,
       'pointerbinds': {
         'pointerup': {
